d1a6180b676b0ab7cb65205ad01fb0021f4425f3
nivetha1698 <nivalogu2@gmail.com>
2025-06-12 18:06:51 +0530

    rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [ :show, :edit, :update ]
-  before_action :authenticate_user!, only: [:upload, :remove]
+  before_action :authenticate_user!, only: [ :upload, :remove ]
 
   def show_current
     @user = current_user
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,32 +8,32 @@ class Ability
 
     user_roles = user.roles.pluck(:name)
 
-    if user_roles.include?('lawyer')
+    if user_roles.include?("lawyer")
       can :read, Case, lawyer_id: user.id
       can :read, Task, lawyer_id: user.id
       can :manage, Appointment, lawyer_id: user.id
-      can :manage, Document, case: { lawyer_id: user.id } 
-      can :manage, DocumentVersions, document: { case: { lawyer_id: user.id } } 
+      can :manage, Document, case: { lawyer_id: user.id }
+      can :manage, DocumentVersions, document: { case: { lawyer_id: user.id } }
       can :create, Notes, lawyer_id: user.id
       cannot :destroy, Case
     end
 
-    if user_roles.include?('admin')
-      can :manage, :all 
+    if user_roles.include?("admin")
+      can :manage, :all
     end
 
-    if user_roles.include?('client')
-      can :read, Case, client_id: user.id  
-      can :read, Document, case: { client_id: user.id } 
-      can :read, DocumentVersions, document: { case: { lawyer_id: user.id } } 
-      can :read, Appointment, lawyer_id: user.id 
+    if user_roles.include?("client")
+      can :read, Case, client_id: user.id
+      can :read, Document, case: { client_id: user.id }
+      can :read, DocumentVersions, document: { case: { lawyer_id: user.id } }
+      can :read, Appointment, lawyer_id: user.id
     end
 
-    if user_roles.include?('judge')
-      can :read, Case 
-      can :manage, Hearings, case: { judge_id: user.id } 
+    if user_roles.include?("judge")
+      can :read, Case
+      can :manage, Hearings, case: { judge_id: user.id }
       can :manage, Judgment, judge_id: user.id # Issue judgments for cases they oversee
-      can :read, Document, case: { status: 'closed' } # View documents of closed cases
+      can :read, Document, case: { status: "closed" } # View documents of closed cases
     end
   end
 end
