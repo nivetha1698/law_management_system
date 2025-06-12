4c031f86da215ef349cef6dd9f39ffc65a31dc8c
nivetha1698 <nivalogu2@gmail.com>
2025-06-12 20:29:04 +0530

    added filters in tasks index page

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,19 @@
 class Task < ApplicationRecord
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_keywords,
+                  against: :title,
+                  associated_against: { court_case: [:title] },
+                  using: { tsearch: { prefix: true } }
+
+  #--------------------------------Associations---------------------------------------------------
+    
     belongs_to :court_case, foreign_key: "case_id", optional: true
     belongs_to :assignee, class_name: "User", foreign_key: "assigned_to"
 
     delegate :name, to: :assignee, allow_nil: true
+    
   #-----------------------------------------Methods----------------------------------------------------------
   def self.ransackable_attributes(auth_object = nil)
     %w[title assignee_name]
