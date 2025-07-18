52f8adc67da5ba7c1c9a1ececc9428f4e8819636
nivetha1698 <nivalogu2@gmail.com>
2025-07-18 10:54:49 +0530

    added small fixes in email templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
-  def send_welcome_email(user)
+  def send_welcome_email(user, token)
     @user = user
+    @token = token
     mail(to: @user.email, subject: "Welcome to Law Management System")
   end
 end
